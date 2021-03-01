226319e52df3f934a55367f1bf67888e1a4fb5e9
camiloserr <camiloserrano@javeriana.edu.co>
2021-03-01 12:06:41 -0500

    finished hello world

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,35 @@
 # asi se escriben los comentarios
 # en las primeras lineas van los imports
+import plots
+import numpy as np
 
 
-print('hello world!')
+# aca se definen las variables globales
+x = 9
+y = 'mineria de datos'
+z = False
+
+
+# asi se definen las funciones
+def saludar(nombre):
+    print("Hola", nombre, "!")
+
+    # si nombre tiene mas de 7 letras:
+    if len(nombre) > 7:
+        print('tienes un nombre largo')
+    else:
+        print('tu nombre no es tan largo...')
+
+    print('tu nombre tiene estas letras:')
+    for i in range(len (nombre)):
+        print(i, nombre[i])
+
+
+if __name__ == "__main__":
+    print('hola mundo!')
+    nombre = input('como es tu nombre? ')
+    saludar(nombre)
+    plots.basic()
+
+
+    
